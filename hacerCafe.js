4d4d356ad6117b1e59625da3d92128282d67fa92
jgongarcia <38234595+jgongarcia@users.noreply.github.com>
2018-04-11 22:47:26 -0300

    El practico hacer cafe que el profe quiere subir

---
+++ hacerCafe.js
@@ -0,0 +1,14 @@
+
+print = console.log;
+
+function makeCafe(cafe,agua){
+    var instrucciones = "instrucciones:";
+    i = instrucciones;
+    respuesta = prompt("desea colocar leche en el cafe?");
+    if (respuesta == "si"){
+        print("Ingrese el porcentaje de leche que desea:");
+        respuesta= prompt("porcnetaje?");
+    }
+}
+
+makeCafe(1,2);
